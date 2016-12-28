808832ac11fafba16ff79449036b38a941d5b41a
Jacob Budzis <jrbudzis@gmail.com>
2016-12-28 00:18:47 -0700

    Added new java files to seperate tasks

--- main.java
+++ main.java
@@ -1,4 +1,18 @@
-public static void main(String[] args){
-
-
+import java.util.ArrayList;
+/* This class executes the code*/
+public class main {
+  public static void main(String[] args) {
+    // Puzzle input. Zeroes represent empty values
+    int[][] puzzle = new int[][]{
+      {0,0,0,0,0,0,0,0,0},
+      {0,0,0,0,0,0,0,0,0},
+      {0,0,0,0,0,0,0,0,0},
+      {0,0,0,0,0,0,0,0,0},
+      {0,0,0,0,0,0,0,0,0},
+      {0,0,0,0,0,0,0,0,0},
+      {0,0,0,0,0,0,0,0,0},
+      {0,0,0,0,0,0,0,0,0},
+      {0,0,0,0,0,0,0,0,0}
+    };
+  }
 }
--- solver.java
+++ solver.java
@@ -1,3 +1,18 @@
-public class solver(){
-  
+/* This class is used to solve the code */
+public class solver{
+
+  private static final int NUM_ROWS = 9;
+  private static final int NUM_COLS = 9;
+
+  // Returns a completed puzzle
+  public static int[][] solver(int[][] puzzle){
+    validNumbers validNumberArray[][] = new validNumbers[NUM_ROWS][NUM_COLS];
+    for (int i=0;i<9;i++){
+      for (int j=0;j<9;j++){
+        validNumberArray[i][j] = new validNumbers();
+      }
+    }
+    return null;
+  }
+
 }
